68439f51b4651309d0afbf62e20cb655dd0c867e
Sergiy Kondratenko <43180380+SergiyKonrad@users.noreply.github.com>
2024-10-31 19:03:48 -0400

    Book.js file with mongoose.Schema created

--- back-api/index.js
+++ back-api/index.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const booksData = require('./data/books.json')
 
+const MONGODB_URI =
+  'mongodb+srv://SergiyKonrad:<db_password>@booklibrarycluster.uekm0.mongodb.net/?retryWrites=true&w=majority&appName=BookLibraryCluster'
+
+mongoose
+// .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(MONGODB_URI)
+
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.error('MongoDB connection error:', err))
+
 const app = express()
 
 app.use(cors())
@@ -13,17 +24,48 @@ function getRandomBook() {
   return randomBook
 }
 
-app.get('/books', (req, res) => {
-  res.json(booksData)
+// app.get('/books', (req, res) => {
+//   res.json(booksData)
+// })
+
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find()
+    res.json(books)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching books' })
+  }
 })
 
-app.get('/random-book', (req, res) => {
-  res.json(getRandomBook())
+// app.get('/random-book', (req, res) => {
+//   res.json(getRandomBook())
+// })
+
+app.get('/random-book', async (req, res) => {
+  try {
+    const books = await Book.find()
+    const randomBook = books[Math.floor(Math.random() * books.length)]
+    res.json(randomBook)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching random book' })
+  }
 })
 
-app.get('/random-book-delayed', (req, res) => {
-  setTimeout(() => {
-    res.json(getRandomBook())
+// app.get('/random-book-delayed', (req, res) => {
+//   setTimeout(() => {
+//     res.json(getRandomBook())
+//   }, 2000)
+// })
+
+app.get('/random-book-delayed', async (req, res) => {
+  setTimeout(async () => {
+    try {
+      const books = await Book.find()
+      const randomBook = books[Math.floor(Math.random() * books.length)]
+      res.json(randomBook)
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching random book' })
+    }
   }, 2000)
 })
 
---
+++ back-api/models/Book.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  year: Number,
+  description: String,
+  isFavorite: { type: Boolean, default: false },
+  source: String,
+  isRandom: { type: Boolean, default: false },
+})
+
+module.exports = mongoose.model('Book', bookSchema)
