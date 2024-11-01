d71913c189dc8855b8511328b3505f36aef09832
Sergiy Kondratenko <43180380+SergiyKonrad@users.noreply.github.com>
2024-11-01 12:29:12 -0400

    'Book' as a 3rd parameter added in mongoose.model('Book', bookSchema, 'Book') with explaination comments

--- back-api/models/Book.js
+++ back-api/models/Book.js
@@ -10,6 +10,11 @@ const bookSchema = new mongoose.Schema({
   isRandom: { type: Boolean, default: false },
 })
 
-module.exports = mongoose.model('Book', bookSchema)
+// module.exports = mongoose.model('Book', bookSchema)
 
-// module.exports = mongoose.model('Book', bookSchema, 'Book')
+/* NB.
+If you define a model with the name Book (as in mongoose.model('Book', bookSchema)), Mongoose automatically assumes the collection name should be plural and lowercase, so it will look for or create a collection named books.
+To override this default behavior, you can specify a third parameter in the mongoose.model function to enforce a specific collection name.
+*/
+
+module.exports = mongoose.model('Book', bookSchema, 'Book')
